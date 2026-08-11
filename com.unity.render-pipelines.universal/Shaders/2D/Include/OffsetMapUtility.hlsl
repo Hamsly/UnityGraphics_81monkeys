62c71f83b6119a04ace877ff97bf31003f3042aa
Cameron Gomez <Cameron@81monkeys.com>
2026-08-11 15:45:01 -0700

    Fixed decode function

--- com.unity.render-pipelines.universal/Shaders/2D/Include/OffsetMapUtility.hlsl
+++ com.unity.render-pipelines.universal/Shaders/2D/Include/OffsetMapUtility.hlsl
@@ -14,12 +14,12 @@ float2 GetScale(float3 OSPosition,float2 lightingUV)
 
 float2 DecodeOffset(float4 color)
 {
-    int g = color.g * 128;
+    int g = color.g * 255;
     float xx = ((color.r * 255) - 127);
     float yy = ((color.b * 255) - 127);
 
-    xx += (g & 0xF) * 256 * sign(xx);
-    yy += (g >> 4 & 0xF) * 256 * sign(yy);
+    xx += (g & 0xF) * 127 * sign(xx);
+    yy += (g >> 4 & 0xF) * 127 * sign(yy);
 
     return float2(round(xx),round(yy));
 }
